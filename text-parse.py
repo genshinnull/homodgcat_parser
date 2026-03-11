84be00c00b8d6ab04e493e6baa02291ead74c0d7
Kinich <>
2026-03-11 16:20:07 +0800

    implement subtitle extract, handle invalid unicode

--- text-parse.py
+++ text-parse.py
@@ -4,13 +4,15 @@ __generated_with = "0.20.4"
 app = marimo.App(width="medium")
 
 with app.setup:
+    import marimo as mo
+
     import os
     import re
     from pathlib import Path
 
-    import marimo as mo
     import orjson
     import polars as pl
+    import pysubs2
     from git import Repo
 
     DATA_PATH = Path(os.environ["DATA_PATH"])
@@ -33,7 +35,7 @@ def _():
 
 
 @app.function
-def extract_textmap(ver: str, lang: str, dir: Path) -> list[pl.DataFrame]:
+def extract_textmap(ver: str, lang: str, dir: Path) -> pl.DataFrame:
     results = []
     files = list(dir.glob("*.json"))
     for file in files:
@@ -50,14 +52,14 @@ def extract_textmap(ver: str, lang: str, dir: Path) -> list[pl.DataFrame]:
                     pl.col.value,
                 )
             )
-    return results
+    return pl.concat(results) if results else pl.DataFrame()
 
 
 @app.function
 def extract_readable(ver: str, lang: str, dir: Path) -> pl.DataFrame:
     data = []
     for file in dir.glob("*.txt"):
-        with open(file) as f:
+        with open(file, errors="replace") as f:
             data.append(
                 {
                     "key": file.stem.split(f"_{lang}")[0],
@@ -76,6 +78,36 @@ def extract_readable(ver: str, lang: str, dir: Path) -> pl.DataFrame:
     )
 
 
+@app.function
+def extract_subtitle(ver: str, lang: str, dir: Path) -> pl.DataFrame:
+    data = []
+    files = []
+    files.extend(dir.glob("*.txt"))
+    files.extend(dir.glob("*.srt"))
+    for file in files:
+        try:
+            sub = pysubs2.load(file)
+            sub_str = "\n".join([line.plaintext for line in sub]).strip()
+        except pysubs2.exceptions.FormatAutodetectionError:
+            sub_str = ""
+        data.append(
+            {
+                "key": file.stem.split(f"_{lang}")[0],
+                "value": sub_str,
+            }
+        )
+    return (
+        pl.DataFrame(data).select(
+            pl.lit(ver).alias("version"),
+            pl.lit("Subtitle").alias("type"),
+            pl.col.key,
+            pl.col.value,
+        )
+        if data
+        else pl.DataFrame()
+    )
+
+
 @app.cell
 def _(versions):
     text_data = {lang: [] for lang in LANGS}
@@ -83,9 +115,9 @@ def _(versions):
         total=len(versions),
         remove_on_exit=True,
         title="Extracting...",
-    ) as bar:
+    ) as _bar:
         for _ver in versions:
-            bar.subtitle = f"Working on Version {_ver['ver']}"
+            _bar.subtitle = f"Working on Version {_ver['ver']}"
             match _ver["type"]:
                 case "REL":
                     _path = DATA_PATH
@@ -94,28 +126,28 @@ def _(versions):
             if _ver.get("hash"):
                 _repo = Repo(_path)
                 assert not _repo.is_dirty()
-                for dir in [
+                for _dir in [
                     Path("TextMap"),
                     Path("Readable"),
                     Path("Subtitle"),
                 ]:
-                    _repo.git.restore("--source", _ver["hash"], dir)
-            # TODO: data ingest
+                    _repo.git.restore("--source", _ver["hash"], _dir)
             for _lang in LANGS:
-                if curr_textmap := extract_textmap(
-                    _ver["ver"], _lang, _path / "TextMap"
-                ):
-                    text_data[_lang].append(pl.concat(curr_textmap))
-                if not (
-                    curr_readable := extract_readable(
+                for _data_df in [
+                    extract_textmap(_ver["ver"], _lang, _path / "TextMap"),
+                    extract_readable(
                         _ver["ver"], _lang, _path / "Readable" / _lang
-                    )
-                ).is_empty():
-                    text_data[_lang].append(curr_readable)
+                    ),
+                    extract_subtitle(
+                        _ver["ver"], _lang, _path / "Subtitle" / _lang
+                    ),
+                ]:
+                    if not _data_df.is_empty():
+                        text_data[_lang].append(_data_df)
             if _ver.get("hash"):
                 _repo.git.clean("-fd")
                 _repo.git.reset("HEAD", "--hard")
-            bar.update()
+            _bar.update()
     text_data = {lang: pl.concat(data) for lang, data in text_data.items()}
     {lang: len(data) for lang, data in text_data.items()}
     return
